2852eedf92732933e9013905ae627d8517d2d8c4
Ryan Boland <bolandryanm@gmail.com>
2015-04-30 15:01:37 -0400

    initial commit

---
+++ lib/sassy_ink/rails.rb
@@ -0,0 +1,7 @@
+require "sassy_ink/rails/version"
+
+module SassyInk
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/sassy_ink/rails/version.rb
@@ -0,0 +1,5 @@
+module SassyInk
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
