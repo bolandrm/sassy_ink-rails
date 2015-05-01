56dac137cca3f497b00cdb98062c17fdc16ebd18
Ryan Boland <bolandryanm@gmail.com>
2015-05-01 12:59:26 -0400

    add assets, reorganize slightly

---
+++ lib/sassy_ink-rails.rb
@@ -0,0 +1,1 @@
+require "sassy_ink/rails"
--- lib/sassy_ink/rails.rb
+++ lib/sassy_ink/rails.rb
@@ -2,6 +2,7 @@ require "sassy_ink/rails/version"
 
 module SassyInk
   module Rails
-    # Your code goes here...
+    class Engine < ::Rails::Engine
+    end
   end
 end
